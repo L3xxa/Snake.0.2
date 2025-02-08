41c43ac508844c48c4e586fdf5c9030592f86468
dzisinho <dzisbogdan26@gmail.com>
2025-02-08 21:25:17 +0200

    Created class_FontManager

--- ConsoleApplication2/Data/FontManager.cpp
+++ ConsoleApplication2/Data/FontManager.cpp
@@ -1,1 +1,10 @@
 ﻿#include "FontManager.h"
+#include <iostream>
+
+bool FontManager::loadFont(const std::string& filename, sf::Font& font) {
+    if (!font.loadFromFile(filename)) {
+        std::cout << "Failed to load font: " << filename << std::endl;
+        return false;
+    }
+    return true;
+}
