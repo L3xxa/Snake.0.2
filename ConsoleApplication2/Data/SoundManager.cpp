339dbc691e7d2388ef173a66ddf74398dd3492d6
dzisinho <dzisbogdan26@gmail.com>
2025-02-10 11:23:57 +0200

    do SoundManager

--- ConsoleApplication2/Data/SoundManager.cpp
+++ ConsoleApplication2/Data/SoundManager.cpp
@@ -1,1 +1,14 @@
 ﻿#include "SoundManager.h"
+#include <iostream>
+
+bool SoundManager::loadSound(const std::string& filename, sf::SoundBuffer& buffer) {
+    if (!buffer.loadFromFile(filename)) {
+        std::cout << "Failed to load sound: " << filename << std::endl;
+        return false;
+    }
+    return true;
+}
+
+void SoundManager::playSound(sf::Sound& sound) {
+    sound.play();
+}
