d687a6ba84c32534989892f67cb6cbd88d2e7919
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-02-10 10:09:10 +0200

    add new item food, and assets

--- ConsoleApplication2/Domain/Food.cpp
+++ ConsoleApplication2/Domain/Food.cpp
@@ -1,1 +1,8 @@
-﻿#include "Food.h"
+﻿
+#include "Food.h"
+#include <cstdlib>
+
+void Food::spawnFood(int width, int height, int size) {
+    x = rand() % (width / size) * size;
+    y = rand() % (height / size) * size;
+}
