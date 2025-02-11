c53044a719a3389f2701babc174bc9c0c069c97b
Alyosha Holovaty <alekseiholovati@gmail.com>
2025-02-11 20:33:51 +0200

    add new item Game

--- ConsoleApplication2/Domain/Game.h
+++ ConsoleApplication2/Domain/Game.h
@@ -1,7 +1,36 @@
-﻿#pragma once
+﻿
+#ifndef GAME_H
+#define GAME_H
 
-class Game
-{
+#include "Snake.h"
+#include "Food.h"
+#include "../Data/SoundManager.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+class Game {
 public:
-    
+    Game(int width, int height, int size);
+    void update();
+    void render(sf::RenderWindow& window) const;
+    bool isGameOver() const;
+    void handleInput(sf::Event& event);
+    void setDirection(int dx, int dy);
+
+    const Snake& getSnake() const { return snake; }
+    const Food& getFood() const { return food; }
+    int getSize() const { return size; }
+
+private:
+    int width, height, size;
+    Snake snake;
+    Food food;
+    bool gameOver;
+    int directionX, directionY;
+    SoundManager soundManager;
+    sf::SoundBuffer eatBuffer, gameOverBuffer;
+    sf::Sound eatSound, gameOverSound;
 };
+
+#endif
+
